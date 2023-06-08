815bf4a1ae897964441f7d3129963c1e21630dc2
AndresSernaSENA <omar_serna@soy.sena.edu.co>
2023-06-08 18:10:51 -0500

    Construcción formulario create Alumnos

--- resources/views/alumnos/create.blade.php
+++ resources/views/alumnos/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layout/template')
 
-@section('title', 'Alumno | Escuela')
+@section('title', 'Registrar Alumno | Escuela')
 
 @section('content')
 
@@ -14,6 +14,31 @@
                 <label for="matricula" class="col-sm-2 col-form-label">Matricula</label>
                 <div class="col-sm-5 ">
                     <input type="text" class="form-control" name="matricula" id="matricula" value="{{old('matricula')}}" required>
+                </div>
+            </div>
+
+            <div class="mb-3 row">
+                <label for="nombre" class="col-sm-2 col-form-label">Nombre completo</label>
+                <div class="col-sm-5 ">
+                    <input type="text" class="form-control" name="nombre" id="nombre" value="{{old('nombre')}}" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="fecha" class="col-sm-2 col-form-label">Fecha de nacimiento</label>
+                <div class="col-sm-5 ">
+                    <input type="date" class="form-control" name="fecha" id="fecha" value="{{old('fecha')}}" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="telefono" class="col-sm-2 col-form-label">Telefono</label>
+                <div class="col-sm-5 ">
+                    <input type="text" class="form-control" name="telefono" id="telefono" value="{{old('telefono')}}" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="email" class="col-sm-2 col-form-label">Correo electronico</label>
+                <div class="col-sm-5 ">
+                    <input type="text" class="form-control" name="email" id="email" value="{{old('email')}}" required>
                 </div>
             </div>
         </form>
